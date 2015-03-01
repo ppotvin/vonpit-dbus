f8c33ea66af9ee775be561397df9cf680400166e
Pascal Potvin <pascal.potvin@gmail.com>
2015-03-01 15:09:50 -0500

    setup.py: add setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='vonpit-dbus',
+    version='0.1',
+    packages=['vonpit_dbus', 'vonpit_dbus.tests'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Pascal Potvin',
+    author_email='pascal.potvin@gmail.com',
+    description='', requires=['six']
+)
