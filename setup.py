787ce84123612b74ba83b03f6d58e3ba97514b63
Pascal Potvin <pascal.potvin@gmail.com>
2015-03-02 21:42:37 -0500

    setup.py: find packages automagically

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # coding: utf-8
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='vonpit-dbus',
     version='0.1',
-    packages=['vonpit_dbus', 'vonpit_dbus.tests'],
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     url='',
-    license='',
+    license='MIT',
     author='Pascal Potvin',
     author_email='pascal.potvin@gmail.com',
     description='',
